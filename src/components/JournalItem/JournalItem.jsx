60e55178786a39a313f85b9afea8ee25b1447d83
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-10-21 19:33:25 +0300

    refactor date format

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -2,11 +2,14 @@ import './JournalItem.css';
 
 export const JournalItem = ({ title, date, text}) => {
 
+	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date);
+	
+
 	return (
 		<div className="journal-item">
 			<h2 className="journal-item__header">{ title }</h2>
 			<div className="journal-item__body">
-				<div className="journal-item__date">{ date.toLocaleString() }</div>
+				<div className="journal-item__date">{ formatedDate }</div>
 				<div className="journal-item__text">{ text }</div>
 			</div>
 

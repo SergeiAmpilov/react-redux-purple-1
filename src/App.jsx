f6cf1398211a47076c0d2cead4e4b8e642dbee4f
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-10-21 16:32:51 +0300

    feat: react demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+  return React.createElement(
+    React.Fragment,
+    {},
+    'Project',
+    React.createElement('p', {}, 'Subtext')
+  );
 
-	return (
-		<div>Проект</div>
-	);
 }
 
 export default App;
+ 

02ccf9336b42c53b82e2c451166f157f459a642b
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-10-21 19:12:01 +0300

    fix some basic styles

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,12 +1,17 @@
 import './JournalItem.css';
 
 export const JournalItem = () => {
+
+	const title = 'Подготовка и обновление курса';
+	const date = new Date();
+	const text = 'Горные походы открывают удивительные горные ландшафты';
+
 	return (
 		<div className="journal-item">
-			<h2 className="journal-item__header">1</h2>
+			<h2 className="journal-item__header">{ title }</h2>
 			<div className="journal-item__body">
-				<div className="journal-item__date">2</div>
-				<div className="journal-item__text">3</div>
+				<div className="journal-item__date">{ date.toLocaleString() }</div>
+				<div className="journal-item__text">{ text }</div>
 			</div>
 
 		</div>

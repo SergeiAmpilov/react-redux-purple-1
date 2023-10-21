a27aa0c21bdf49731925648fa83fe94dbe779876
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-10-21 18:57:31 +0300

    feat - basic markdown for journal item

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
 import { Button } from './components/Button/Button';
+import { JournalItem } from './components/JournalItem/JournalItem';
 
 function App() {
   
   return (
-    <>
+    <div className="container">
       <h1>Project</h1>
       <p>subtitle</p>
       <Button />
-    </>
+      <JournalItem />
+      <JournalItem />
+      <JournalItem />
+
+    </div>
   );
 
 }

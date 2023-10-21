ba96afcbae090efab46ee9a915c1b2934be9e97d
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-10-21 22:56:18 +0300

    feat: use state hook

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import './Button.css';
 
 export const Button = () => {
 
+	const [text, setText] = React.useState('Сохранить');
+
 	const handleClick = () => {
 		console.log('hw');
+		setText('Закрыть');
 	};
 
 	return (
 		<button onClick={handleClick} className='button accent'>
-			button
+			{ text }
 		</button>
 	);
 };
